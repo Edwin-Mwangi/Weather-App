1d49ef9884816e69775e3289b41d93a0f45c24e7
Edwin Mwangi <edwinmwangi138@gmail.com>
2021-10-04 20:54:21 +0300

    new mods

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 ///we are going to implement async JS in a weather app
 //key in the location and receive the weather of the area 
+//You need to register to accuweather and youre given a certainkey
 
 //page behaviour
---
+++ scripts/forecast.js
@@ -0,0 +1,9 @@
+// for interacting with the weather API...for free accounts only 50 requests per day...
+//...to overcome delete an app and create another and you'll be given another key
+
+
+//Api key
+const key = 'D8bGw3mx12SZo87n5MWtX0Nz1kGnDQhG';
+
+//2 requests to be made.One to get the city info endpoint to acquire city code
+//the sendo is to send city code to weather conditions Api to get weather conditions to that area 
